e4a864642e1e7e76a9854cb5752d11857179f774
Etidal-Kebsi <etidalkebsi@gmail.com>
2021-12-17 00:25:35 +0100

    Initial Commit

---
+++ src/Server.java
@@ -0,0 +1,22 @@
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+public class Server {
+
+static ArrayList<Compte> listeComptes ;
+	
+	public static void main(String[] args) throws IOException {
+		listeComptes = new ArrayList<Compte>();
+		
+		ServerSocket soc = new ServerSocket(5000);
+		System.out.println("server creer");
+		while(true) {
+			
+		Socket sc =soc.accept();
+		Traitement tr = new Traitement(sc);
+		tr.start();
+		}
+	}
+}
